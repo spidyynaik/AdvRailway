1c9bb652ec093247b2d2b2d56729848dd2072fac
spidyynaik <128451864+spidyynaik@users.noreply.github.com>
2023-06-01 17:38:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/spidyynaik/AdvRailway /spidyauto
+  git clone https://github.com/spidyynaik/myauto.git /Spider
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /spidyauto
+  git clone $UPSTREAM_REPO /Spider
 fi
-cd /spidyauto
+cd /Spider
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
